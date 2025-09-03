f493790a3a78cfcc48ed08fdbd3726f86d76c5ef
tushar-19459 <19459tusharraj@gmail.com>
2025-09-03 23:37:07 +0530

    work 1 done

--- work_1/work_1/pipelines.py
+++ work_1/work_1/pipelines.py
@@ -6,8 +6,29 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class Work1Pipeline:
+    def __init__(self):
+        self.create_Connection()
+        self.CreateTable()
+
+    def create_Connection(self):
+        self.connection = sqlite3.connect('Work1.db')
+        self.current = self.connection.cursor()
+        pass
+
+    def CreateTable(self):
+        self.current.execute(""" drop table if exists work1 """)
+        self.current.execute(""" create table work1(job_title text,location text,Skills text,date text,category text)""")
+        pass
+
+    def Insert(self,item):
+        self.current.execute(""" insert into work1(job_title ,location ,Skills ,date ,category ) values (?,?,?,?,?)""",(item['job_title'],item['location'],item['Skills'],item['date'],item['category']))
+        self.connection.commit()
+        pass
+
     def process_item(self, item, spider):
+        self.Insert(item)
         return item
+
--- work_1/work_1/spiders/spider.py
+++ work_1/work_1/spiders/spider.py
@@ -19,15 +19,21 @@ class SpiderSpider(scrapy.Spider):
     #         }
             
     # using the normal items and yield 
+    
     def parse(self, response):
-        items = Work1Item()
         ol = response.xpath('//ol/li')
 
         for i in ol:
+            items = Work1Item()
             items['job_title'] = i.xpath('.//*[@class="listing-company-name"]/a/text()').extract()[0]
             items['location'] = i.xpath('.//*[@class="listing-location"]/a/text()').extract()[0]
-            items['Skills'] = i.xpath('.//*[@class="listing-job-type"]/a/text()').extract()
+            items['Skills'] = ", ".join(i.xpath('.//*[@class="listing-job-type"]/a/text()').extract())
             items['date'] = i.xpath('.//*[@class="listing-posted"]/time/text()').extract()[0]
             items['category'] = i.xpath('.//*[@class="listing-company-category"]/a/text()').extract()[0]
-            yield items
-            
+            yield items 
+        
+        next = response.xpath('//*[@class="next"]/a/@href').get()
+        if next != "":
+            new = response.urljoin(next)
+            yield response.Request(new)
+        
